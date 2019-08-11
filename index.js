c5ce42b8c292fa3745d44dfbc0d45bd290c58d72
Kevin Wang <kwangsan@gmail.com>
2019-08-10 21:53:56 -0400

    extract `actions/index.js`; update query with args

--- index.js
+++ index.js
@@ -1,167 +1,4 @@
-require("dotenv").config();
-const AWS = require("aws-sdk");
-
-AWS.config.update({
-  region: "us-east-1",
-  // endpoint: "http://localhost:8000",
-  // endpoint: "https://dynamodb.us-east-1.amazonaws.com",
-  // accessKeyId default can be used while using the downloadable version of DynamoDB.
-  // For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
-  accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
-  // secretAccessKey default can be used while using the downloadable version of DynamoDB.
-  // For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
-  secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY
-});
-
-// console.log(AWS.config);
-// console.log("dynamodb", dynamodb);
-
-/**
- * Locking the API Version
- * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html
- */
-const dynamodb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-function createMovies() {
-  var params = {
-    TableName: "Movies",
-    KeySchema: [
-      { AttributeName: "year", KeyType: "HASH" },
-      { AttributeName: "title", KeyType: "RANGE" }
-    ],
-    AttributeDefinitions: [
-      { AttributeName: "year", AttributeType: "N" },
-      { AttributeName: "title", AttributeType: "S" }
-    ],
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 5,
-      WriteCapacityUnits: 5
-    }
-  };
-
-  dynamodb.createTable(params, (err, data) => {
-    if (err) {
-      console.error(
-        "Unable to create table: " + "\n" + JSON.stringify(err, undefined, 2)
-      );
-    } else {
-      console.log(
-        "Created table: " + "\n" + JSON.stringify(data, undefined, 2)
-      );
-    }
-  });
-}
-// createMovies();
-
-/**
- * createItem()
- */
-function createItem() {
-  var params = {
-    TableName: "Movies",
-    Item: {
-      year: 2015,
-      title: "The Big New Movie",
-      info: {
-        plot: "Nothing happens at all.",
-        rating: 0
-      }
-    }
-  };
-  docClient.put(params, function(err, data) {
-    if (err) {
-      console.error(
-        "Unable to add item: " + "\n" + JSON.stringify(err, undefined, 2)
-      );
-    } else {
-      console.log(
-        "PutItem succeeded: " + "\n" + JSON.stringify(data, undefined, 2)
-      );
-    }
-  });
-}
-// createItem();
-
-/**
- * readItem()
- */
-function readItem() {
-  var table = "Movies";
-  var year = 2015;
-  var title = "The Big New Movie";
-
-  var params = {
-    TableName: table,
-    Key: {
-      year: year,
-      title: title
-    }
-  };
-
-  let foo;
-  return docClient
-    .get(params, (err, data) => {
-      if (err) {
-        console.log(
-          "Unable to read item: " + "\n" + JSON.stringify(err, undefined, 2)
-        );
-      } else {
-        console.log(
-          "GetItem succeeded: " + "\n" + JSON.stringify(data, undefined, 2)
-        );
-      }
-    })
-    .promise()
-    .then(promise => {
-      console.log(".then.promise", promise);
-      return [promise];
-    });
-}
-
-/**
- * updateItem
- */
-function updateItem() {
-  var table = "Movies";
-
-  var year = 2015;
-  var title = "The Big New Movie";
-
-  // Update the item, unconditionally,
-
-  var params = {
-    TableName: table,
-    Key: {
-      year: year,
-      title: title
-    },
-    UpdateExpression: "set info.rating = :r, info.plot=:p, info.actors=:a",
-    ExpressionAttributeValues: {
-      ":r": 5.5,
-      ":p": "Everything happens all at once.",
-      ":a": ["Larry", "Moe", "Curly"]
-    },
-    ReturnValues: "UPDATED_NEW"
-  };
-
-  console.log("Updating the item...");
-  docClient.update(params, function(err, data) {
-    if (err) {
-      console.error(
-        "Unable to update item. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-    } else {
-      console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
-    }
-  });
-}
-// updateItem();
-
-/**
- * =====================================================>
- */
+const { createItem, createMovies, readItem, updateItem } = require("./actions");
 
 const { ApolloServer, gql } = require("apollo-server");
 const movies = [
@@ -173,6 +10,16 @@ const movies = [
         plot: "Nothing happens at all."
       },
       title: "The Big New Movie"
+    }
+  },
+  {
+    Item: {
+      year: 2015,
+      info: {
+        rating: 0,
+        plot: "Boop."
+      },
+      title: "Another New Movie"
     }
   }
 ];
@@ -211,7 +58,7 @@ const typeDefs = gql`
   }
 
   type Query {
-    movies: [Movie]
+    movie(title: String!): Movie
     books: [Book]
   }
 `;
@@ -220,8 +67,9 @@ const typeDefs = gql`
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    movies: (obj, args, context, info) => {
-      return readItem();
+    movie: (obj, args, context, info) => {
+      console.log("args", args);
+      return readItem({ title: args.title });
     },
     books: () => books
   }
