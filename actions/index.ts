c6d80ef84127f95b52b89aa02a7196f45e488830
Kevin Wang <kwangsan@gmail.com>
2019-08-11 23:24:04 -0400

    clean up actions

--- actions/index.ts
+++ actions/index.ts
@@ -1,36 +1,12 @@
-require("dotenv").config();
-const AWS = require("aws-sdk");
-
-// Safe to say that running `now` updates `process.env.NODE_ENV` to `production`
-const isEnvProduction = process.env.NODE_ENV === "production";
-const isEnvDevelopment = process.env.NODE_ENV === "development";
-
-const USERS = "Users";
-
-AWS.config.update({
-  region: "us-east-1",
-  endpoint: isEnvProduction
-    ? "https://dynamodb.us-east-1.amazonaws.com"
-    : "http://localhost:8000",
-  // endpoint: "https://dynamodb.us-east-1.amazonaws.com",
-  // accessKeyId default can be used while using the downloadable version of DynamoDB.
-  // For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
-  accessKeyId: isEnvProduction
-    ? process.env.MY_AWS_ACCESS_KEY_ID
-    : "fakeMyKeyId",
-  // secretAccessKey default can be used while using the downloadable version of DynamoDB.
-  // For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
-  secretAccessKey: isEnvProduction
-    ? process.env.MY_AWS_SECRET_ACCESS_KEY
-    : "fakeSecretAccessKey"
-});
-
 /**
- * Locking the API Version
- * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html
+ * TODO:
+ *
+ * DELETE THIS FILE
  */
-const dynamodb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
-const docClient = new AWS.DynamoDB.DocumentClient();
+
+import { dynamodb, docClient } from "../src/context/aws";
+
+export const USERS = "Users";
 
 export function createUsersTable() {
   var params = {
