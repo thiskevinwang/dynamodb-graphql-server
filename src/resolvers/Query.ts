27499b3560627defe228b31955cec364a5ef6ce7
Kevin Wang <kwangsan@gmail.com>
2019-08-14 00:16:06 -0400

    improve Query return values

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,5 +1,46 @@
 const USERS = "Users";
 
+const getUser = async (obj, args, context, info) => {
+  const { username, id }: { username: string; id: number } = args;
+  const { docClient } = context;
+  const params = {
+    TableName: USERS,
+    Key: {
+      id: id,
+      username: username
+    }
+  };
+  return docClient
+    .get(params, function(err, data) {
+      if (err) {
+        console.error(
+          "Unable to get item. Error JSON:",
+          JSON.stringify(err, null, 2)
+        );
+      } else {
+        // console.log("getItem succeeded:", JSON.stringify(data, null, 2));
+      }
+    })
+    .promise()
+    .then(res => {
+      console.log(res);
+      /**
+       * TO AVOID THIS, YOU CAN DO
+       */
+      // const { Item } = res;
+      // return {
+      //   attributes: Item.attributes },
+      //   id: Item.id,
+      //   username: Item.username
+      // };
+      /**
+       * THIS
+       */
+      const { Item: User } = res;
+      return User;
+    });
+};
+
 const getUsers = async (obj, args, context, info) => {
   const table = USERS;
   const { docClient } = context;
@@ -34,8 +75,10 @@ const getUsers = async (obj, args, context, info) => {
     .promise()
     .then(result => {
       console.log("result", result);
-      return result;
+      const { Items, Count, ScannedCount } = result;
+      // Restructure the dynamodb response for better graphql handling
+      return { Users: Items, Count, ScannedCount };
     });
 };
 
-export default { getUsers };
+export default { getUser, getUsers };
