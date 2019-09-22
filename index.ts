6f5c6583ba3d2924610db432d5b191816113fa4f
Kevin Wang <kwangsan@gmail.com>
2019-09-22 13:12:37 -0400

    chalk message on startup

--- index.ts
+++ index.ts
@@ -1,8 +1,22 @@
 import { ApolloServer, PubSub } from "apollo-server";
+import chalk from "chalk";
 
 import { typeDefs } from "./src/schema.graphql";
 import { resolvers } from "./src/resolvers";
 import { AWS, dynamodb, docClient } from "./src/context/aws";
+
+const highlight = chalk.bold.underline.blueBright;
+const name = "Kevin";
+
+console.log(chalk`
+  👋 I'm {bold ${name}}.
+  This is a serverless server, built with
+  - {hex('#3F20BA') Apollo}
+  - {white.bgHex('#000000') Zeit Now}
+  - {hex('#CC6600') AWS DynamoDb}
+  - {white.bgHex('#007ACC') TypeScript}
+  - {hex('#E10098') GraphQL}
+`);
 
 // For subscriptions to work, the same single instance of
 // PubSub should be in the ApolloServer context object
@@ -29,6 +43,6 @@ const server = new ApolloServer({
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url, subscriptionsUrl }) => {
-  console.log(`🚀 Server ready at ${url}`);
-  console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
+  console.log(`🚀 Server ready at ${highlight(url)}`);
+  console.log(`🚀 Subscriptions ready at ${highlight(subscriptionsUrl)}`);
 });
