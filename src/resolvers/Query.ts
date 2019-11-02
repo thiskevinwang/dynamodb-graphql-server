24be841cc4ddf9dba40eeef76049a19eeee5fc8b
Kevin Wang <kwangsan@gmail.com>
2019-11-02 17:02:49 -0400

    add getIps quer

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,6 @@
 import chalk from "chalk";
 
-import { PAGES, USER_ADDED, USER_UPDATED } from "./index";
+import { PAGES, USER_ADDED, USER_UPDATED, IPS } from "./index";
 
 const green = chalk.underline.greenBright;
 
@@ -98,4 +98,52 @@ const getPages = async (obj, args, context, info) => {
     });
 };
 
-export default { getPage, getPages };
+const getIps = async (obj, args, context, info) => {
+  const table = IPS;
+  const { docClient } = context;
+  const { fieldName, parentType } = info;
+
+  const params = {
+    TableName: table
+    // Key: {
+    //   year: year,
+    //   title: title
+    // },
+    // UpdateExpression: "set info.rating = :r, info.plot=:p, info.actors=:a",
+    // ExpressionAttributeValues: {
+    //   ":r": 5.5,
+    //   ":p": "Everything happens all at once.",
+    //   ":a": ["Larry", "Moe", "Curly"]
+    // },
+    // ReturnValues: "UPDATED_NEW"
+  };
+
+  console.log("Scanning for items...");
+  return docClient
+    .scan(
+      params
+      // 🚫
+      // function(err, data) {
+      //   if (err) {
+      //     console.error(
+      //       "Unable to update item. Error JSON:",
+      //       JSON.stringify(err, null, 2)
+      //     );
+      //   } else {
+      //     console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+      //   }
+      // }
+    )
+    .promise()
+    .then(res => {
+      console.group(green(`${chalk.bold(parentType)}: ${fieldName}`));
+      console.log(res);
+      console.log("\n");
+      console.groupEnd();
+      const { Items, Count, ScannedCount } = res;
+      // Restructure the dynamodb response for better graphql handling
+      return { Ips: Items, Count, ScannedCount };
+    });
+};
+
+export default { getPage, getPages, getIps };
