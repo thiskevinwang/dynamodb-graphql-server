312df88dea8bcb56e8a41de2d29add6db87ca912
Kevin Wang <kwangsan@gmail.com>
2019-11-02 16:55:30 -0400

    IPS tablename const

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -3,6 +3,7 @@ import Mutation from "./Mutation";
 import Subscription from "./Subscription";
 
 export const PAGES = "Pages";
+export const IPS = "IPS";
 export const USER_ADDED = "USER_ADDED";
 export const USER_UPDATED = "USER_UPDATED";
 
