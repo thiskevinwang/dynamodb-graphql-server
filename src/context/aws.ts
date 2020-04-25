115d01e295ae6c31f9aaa2a398dc5d94e5dc0ba3
Kevin Wang <kwangsan@gmail.com>
2020-04-25 11:23:21 -0400

    generate types correctly in aws.ts

--- src/context/aws.ts
+++ src/context/aws.ts
@@ -1,16 +1,14 @@
-require("dotenv").config();
-const AWS = require("aws-sdk");
+require("dotenv").config()
+import AWS from "aws-sdk"
 
 // Safe to say that running `now` updates `process.env.NODE_ENV` to `production`
-const isEnvProduction = process.env.NODE_ENV === "production";
-const isEnvDevelopment = process.env.NODE_ENV === "development";
+const isEnvProduction = process.env.NODE_ENV === "production"
+const isEnvDevelopment = process.env.NODE_ENV === "development"
 
 AWS.config.update(
   isEnvProduction
     ? {
         region: "us-east-1",
-        endpoint: "https://dynamodb.us-east-1.amazonaws.com",
-        // endpoint: "https://dynamodb.us-east-1.amazonaws.com",
         // accessKeyId default can be used while using the downloadable version of DynamoDB.
         // For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
         accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
@@ -19,24 +17,33 @@ AWS.config.update(
         secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
       }
     : {
-        endpoint: "http://localhost:8081",
+        region: "localhost",
         accessKeyId: "zeaq37",
         secretAccessKey: "eyw4ab",
-        region: "localhost",
       }
-);
+)
 
 /**
  * Locking the API Version
  * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html
  */
-const dynamodb = new AWS.DynamoDB({
-  apiVersion: "2012-08-10",
-  endpoint: "http://localhost:8081",
-  accessKeyId: "zeaq37",
-  secretAccessKey: "eyw4ab",
-  region: "localhost",
-});
-const docClient = new AWS.DynamoDB.DocumentClient();
+const dynamodb = new AWS.DynamoDB(
+  isEnvProduction
+    ? {
+        apiVersion: "2012-08-10",
+        region: "us-east-1",
+        endpoint: "https://dynamodb.us-east-1.amazonaws.com",
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+      }
+    : {
+        apiVersion: "2012-08-10",
+        region: "localhost",
+        endpoint: "http://localhost:8081",
+        accessKeyId: "zeaq37",
+        secretAccessKey: "eyw4ab",
+      }
+)
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-export { AWS, dynamodb, docClient };
+export { AWS, dynamodb, docClient }
