f5fa4fa0ca46a289c0c3b5eeb11b77b12d236085
Kevin Wang <kwangsan@gmail.com>
2020-04-25 15:43:16 -0400

    remove subscriptions 2

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,16 +1,12 @@
 import * as Query from "./Query"
 import * as Mutation from "./Mutation"
-import Subscription from "./Subscription"
 
 export const PAGES = "Pages"
 export const IPS = "IPS"
-export const USER_ADDED = "USER_ADDED"
-export const USER_UPDATED = "USER_UPDATED"
 
 export const resolvers = {
   Query,
   Mutation,
-  Subscription,
 }
 
 export default resolvers
