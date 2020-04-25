d4c265743e04607c6143c59221bfa133b3adedff
Kevin Wang <kwangsan@gmail.com>
2020-04-25 13:31:29 -0400

    update ResolverFn

--- src/resolvers/ResolverFn.d.ts
+++ src/resolvers/ResolverFn.d.ts
@@ -1,8 +1,10 @@
+import { GraphQLResolveInfo } from "graphql"
+// import { IFieldResolver } from "graphql-tools"
 import { Context } from "../../index"
 
 export type ResolverFn<R = any, A = any> = (
   parent: any,
   args: A,
   context: Context,
-  info: any
+  info: GraphQLResolveInfo
 ) => Promise<R>
