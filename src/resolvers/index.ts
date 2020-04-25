518735fadcac5ca82c31fcf8277d1f330514dd4c
Kevin Wang <kwangsan@gmail.com>
2020-04-25 11:23:53 -0400

    format resolvers

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,16 +1,16 @@
-import Query from "./Query";
-import Mutation from "./Mutation";
-import Subscription from "./Subscription";
+import Query from "./Query"
+import Mutation from "./Mutation"
+import Subscription from "./Subscription"
 
-export const PAGES = "Pages";
-export const IPS = "IPS";
-export const USER_ADDED = "USER_ADDED";
-export const USER_UPDATED = "USER_UPDATED";
+export const PAGES = "Pages"
+export const IPS = "IPS"
+export const USER_ADDED = "USER_ADDED"
+export const USER_UPDATED = "USER_UPDATED"
 
 export const resolvers = {
   Query,
   Mutation,
-  Subscription
-};
+  Subscription,
+}
 
-export default resolvers;
+export default resolvers
