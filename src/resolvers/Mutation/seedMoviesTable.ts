cbaf7a275475a43445d2463c3641cf3a08ed5c35
Kevin Wang <kwangsan@gmail.com>
2020-04-26 10:54:56 -0400

    add spinner to 'seedMoviesTable'

--- src/resolvers/Mutation/seedMoviesTable.ts
+++ src/resolvers/Mutation/seedMoviesTable.ts
@@ -1,7 +1,10 @@
 import DynamoDB from "aws-sdk/clients/dynamodb"
+import ora from "ora"
 
 import { ResolverFn } from "resolvers/ResolverFn"
 import allMovies from "./__mocks__/moviedata.json"
+
+const spinner = ora("Seeding Movies")
 
 export const seedMoviesTable: ResolverFn = async (
   obj,
@@ -10,27 +13,36 @@ export const seedMoviesTable: ResolverFn = async (
   { fieldName, parentType }
 ) => {
   const { docClient } = context
-  allMovies.forEach(movie => {
-    const params: DynamoDB.DocumentClient.PutItemInput = {
-      TableName: "Movies",
-      Item: {
-        year: movie.year,
-        title: movie.title,
-        info: movie.info,
-      },
-    }
+  let count = 0
+  spinner.start()
 
-    docClient
-      .put(params)
-      .promise()
-      .then(res => {
-        console.info(parentType.name, fieldName, res)
-        return res.Attributes
-      })
-      .catch(err => {
-        console.error(parentType.name, fieldName, err.message)
-        throw err
-      })
+  return await Promise.all([
+    ...allMovies.map(async movie => {
+      const params: DynamoDB.DocumentClient.PutItemInput = {
+        TableName: "Movies",
+        Item: {
+          year: movie.year,
+          title: movie.title,
+          info: movie.info,
+        },
+      }
+
+      return await docClient
+        .put(params)
+        .promise()
+        .then(res => {
+          spinner.text = `Seeding Movies - ${++count}`
+          // console.info(parentType.name, fieldName, res)
+          return res.Attributes
+        })
+        .catch(err => {
+          // console.error(parentType.name, fieldName, err.message)
+          throw err
+        })
+    }),
+  ]).then(() => {
+    spinner.succeed(`Done - Added ${count} movies`)
+    return "ok"
   })
-  return "ok"
+  // return "ok"
 }
