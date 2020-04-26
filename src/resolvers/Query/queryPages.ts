5f75ed5877d59fa361226b3b060aed1b9fc1d0b2
Kevin Wang <kwangsan@gmail.com>
2020-04-26 10:55:10 -0400

    add queryPages

---
+++ src/resolvers/Query/queryPages.ts
@@ -0,0 +1,39 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb"
+
+import { ResolverFn } from "resolvers/ResolverFn"
+
+import { PAGES } from "../index"
+
+export const queryPages: ResolverFn = async (
+  obj,
+  args,
+  { docClient },
+  { fieldName, parentType }
+) => {
+  const params: DocumentClient.QueryInput = {
+    TableName: PAGES,
+    KeyConditionExpression:
+      "#id = :id and #location between :letter1 and :letter2",
+    ExpressionAttributeNames: {
+      "#id": "id",
+      "#location": "location",
+    },
+    ExpressionAttributeValues: {
+      ":id": 2,
+      ":letter1": "0",
+      ":letter2": "Z",
+    },
+    Limit: 10,
+  }
+  return docClient
+    .query(params)
+    .promise()
+    .then(res => {
+      console.info(parentType.name, fieldName, res)
+      return res.Items
+    })
+    .catch(err => {
+      console.error(parentType.name, fieldName, err.message)
+      throw err
+    })
+}
